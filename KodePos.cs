fe98ef71aee2c4d02b132ef510558ac8c7b9bc62
raffarizzy <raffarizkyf@gmail.com>
2025-03-09 10:25:54 +0700

    menambahkan implementasi dengan table-driven

---
+++ KodePos.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul4_103022330138
+{
+    class KodePos
+    {
+        private static string[] kota = { "Batununggal", "Kujangsari", "Mengger", "Wates", "Cijaura", "Jatisari", "Margasari", "Sekejati", "Kebonwaru", "Maleer", "Samoja" };
+        private static int[] kodePosKota = { 40266, 40287, 40267, 40256, 40287, 40286, 40286, 40272, 40274, 40273, 40275 };
+
+        public static int GetKodePos(string namaKota)
+        {
+            for (int i = 0; i < kota.Length; i++)
+            {
+                if (kota[i] == namaKota)
+                {
+                    return kodePosKota[i];
+                }
+            }
+            throw new ArgumentException("Kode pos tidak ditemukan untuk kota: " + namaKota);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace tpmodul4_103022330138
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kode Pos Kujangsari: " + KodePos.GetKodePos("Kujangsari"));
+            Console.WriteLine("Kode Pos Maleer: " + KodePos.GetKodePos("Maleer"));
+
+            try
+            {
+                Console.WriteLine("Kode Pos New York: " + KodePos.GetKodePos("NewYork"));
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
